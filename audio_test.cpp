74ed1f40a8f1b582cc8cccbb1a073456ac565f3b
Jordan Dunn <jordan@nodetwo.net>
2013-10-20 03:46:45 -0500

    Cleaning it up a bit.

--- audio_test.cpp
+++ audio_test.cpp
@@ -2,29 +2,26 @@
 #include <string>
 #include <fstream>
 
-// Load SFML Audio header
 #include <SFML/Audio.hpp>
 
-int main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
-		std::cout << "Audio Test 1.0" << std::endl;
-		std::cout << "Usage is: audio_test <file>" << std::endl;
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		cout << "Audio Test 1.0" << endl;
+		cout << "Usage is: audio_test <file>" << endl;
 	} else {
-		std::string song = argv[1];
+		string song = argv[1];
 
 		sf::Music Music;
-		if (!Music.OpenFromFile(song))
-		{
-			return (-1);
+		if (!Music.OpenFromFile(song)) {
+			return EXIT_FAILURE;
 		}
 		Music.Play();
 
-		std::cout << "Now playing " << song << std::endl;
-		std::cout << "Hit [ENTER] to exit program" << std::endl;
-		std::cin.ignore().get();
+		cout << "Now playing " << song << endl;
+		cout << "Hit [ENTER] to exit program" << endl;
+		cin.ignore().get();
 	}
-
 	return (0);
 }
