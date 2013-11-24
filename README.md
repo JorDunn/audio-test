a0978808c68e9eb3183dc43bbc54e6bc1cc12fda
Jordan Dunn <jordan@nodetwo.net>
2013-11-24 02:14:17 -0600

    Updating for SFML 2.1 and including osx binary

---
+++ README.md
@@ -0,0 +1,12 @@
+Its pretty simple to use:
+
+	./audio_test <file>
+
+Requires SFML 2.1 <http://www.sfml-dev.org>.
+
+To compile, just type:
+
+	g++ -o audio_test audio_test.cpp -lsfml-system -lsfml-audio
+
+This little program should compile just fine on OSX and Linux.
+Also included is a OSX 64bit binary with a linux version on its way. Enjoy!
--- audio_test.cpp
+++ audio_test.cpp
@@ -1,27 +1,34 @@
+/*
+	Audio Test
+	Jordan Dunn (c) 2012-2013
+	dunnj@my.normandale.edu
+*/
+
 #include <iostream>
 #include <string>
-#include <fstream>
 
+#include <SFML/System.hpp>
 #include <SFML/Audio.hpp>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
-		cout << "Audio Test 1.0" << endl;
+		cout << "Audio Test 1.0.1" << endl;
 		cout << "Usage is: audio_test <file>" << endl;
-	} else {
+	}
+	else {
 		string song = argv[1];
 
-		sf::Music Music;
-		if (!Music.OpenFromFile(song)) {
-			return EXIT_FAILURE;
+		sf::Music music;
+		if (!music.openFromFile(song)) {
+			return -1;
 		}
-		Music.Play();
+		music.play();
 
 		cout << "Now playing " << song << endl;
 		cout << "Hit [ENTER] to exit program" << endl;
 		cin.ignore().get();
 	}
-	return (0);
+	return 0;
 }
