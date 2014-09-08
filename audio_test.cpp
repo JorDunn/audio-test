9e55063600ab26592bdb2dae99a04cee12c01bc9
Jordan Dunn <jordan@nodetwo.net>
2014-09-08 12:39:24 -0500

    Working on windows support. Also, converted tabs to spaces.

--- audio_test.cpp
+++ audio_test.cpp
@@ -1,7 +1,7 @@
 /*
-	program: audio_test
-	author: Jordan Dunn <jordan@nodetwo.net>
-	copyright (c) 2012-2014
+    program: audio_test
+    author: Jordan Dunn <jordan@nodetwo.net>
+    copyright (c) 2012-2014
 */
 
 #include <iostream>
@@ -19,38 +19,38 @@ void playFile(string music_file);
 void help();
 
 int main(int argc, char *argv[]) {
-	argParser(argc, argv);
+    argParser(argc, argv);
 
-	return 0;
+    return 0;
 }
 
 void argParser(int argc, char *argv[]) {
-	if(!strcmp(argv[1], "-h")) {
-		help();
-		return;
-	}
-	else if(!strcmp(argv[1], "-f")) {
-		playFile(argv[2]);
-	}
-	else {
-		help();
-		return;
-	}
+    if(!strcmp(argv[1], "-h")) {
+        help();
+        return;
+    }
+    else if(!strcmp(argv[1], "-f")) {
+        playFile(argv[2]);
+    }
+    else {
+        help();
+        return;
+    }
 }
 
 void playFile(string music_file) {
-	sf::Music music;
-	if (!music.openFromFile(music_file)) {
-		return;
-	}
-	music.play();
+    sf::Music music;
+    if (!music.openFromFile(music_file)) {
+        return;
+    }
+    music.play();
 
-	cout << "Now playing " << music_file << endl;
-	cout << "Hit [ENTER] to exit program" << endl;
-	cin.ignore();
+    cout << "Now playing " << music_file << endl;
+    cout << "Hit [ENTER] to exit program" << endl;
+    cin.ignore();
 }
 
 void help() {
-	cout << "Audio Test " << VERSION << endl;
-	cout << "Usage is: audio_test -f <file>" << endl;
+    cout << "Audio Test " << VERSION << endl;
+    cout << "Usage is: audio_test -f <file>" << endl;
 }
